0ce6816093ca838def338b099c567db983fce2ad
Luis Jimenez <ljimenezb83@gmail.com>
2023-02-16 18:17:06 -0500

    Updated SQL queries

--- little_lemon_queries.sql
+++ little_lemon_queries.sql
@@ -58,25 +58,52 @@ WHERE BookingID = UpdateID;
 CALL UpdateBooking(6, 1);
 
 SELECT * FROM Bookings;
-
+DROP PROCEDURE AddBooking;
 -- a procedure to add a booking
-CREATE PROCEDURE AddBooking(IN AddBookingID INT, IN AddBookingDate DATE, IN AddTableNo INT, IN AddCustomerID INT, IN AddEmployeeNum INT)
+DELIMITER //
+CREATE PROCEDURE AddBooking (IN AddBookingID INT, IN AddBookingDate DATE, IN AddTableNo INT, IN AddCustomerID INT, IN AddEmployeeNum INT)
+BEGIN
 INSERT INTO Bookings (BookingID, BookingDate, TableNumber, CustomerID, EmployeeID)
 VALUES (AddBookingID, AddBookingDate, AddTableNo, AddCustomerID, AddEmployeeNum);
+SELECT CONCAT("New Booking ID ", AddBookingID, " Added.") AS Confirmation;
+END//
+DELIMITER ;
 
 -- call the procedure 
 CALL AddBooking(5, "2022-12-30", 4, 3, 1);
+
+DELIMITER //
+CREATE PROCEDURE MakeBooking (booking_id INT, customer_id INT, table_no int, booking_date date)
+BEGIN
+INSERT INTO Bookings (BookingID, BookingDate, TableNumber, CustomerID) 
+VALUES (booking_id, booking_date, table_no, customer_id);
+SELECT "New booking added" AS "Confirmation";
+END//
+DELIMITER ;
+
+CALL MakeBooking(5, "2022-12-30", 4, 3, 1);
 
 -- check the bookings table
 SELECT * FROM Bookings;
 
 -- a procedure to check whether a table in the restaurant is already booked
 DROP PROCEDURE IF EXISTS CheckBooking;
-CREATE PROCEDURE CheckBooking(IN CheckBookingDate DATE, IN CheckTable INT)
-SELECT IF((BookingDate = CheckBookingDate AND tableNumber = CheckTable), 
-		CONCAT("Table ",CheckTable, " is already booked."), CONCAT("Table ",CheckTable, " is available.")) AS "Booking Status"
-FROM Bookings
-WHERE BookingDate = CheckBookingDate AND TableNumber = CheckTable;
+
+-- a stored procedure to check if a table is booked on a given date
+DELIMITER //
+CREATE PROCEDURE CheckBooking (booking_date DATE, table_number INT)
+BEGIN
+DECLARE bookedTable INT DEFAULT 0;
+ SELECT COUNT(bookedTable)
+    INTO bookedTable
+    FROM Bookings WHERE BookingDate = booking_date and TableNumber = table_number;
+    IF bookedTable > 0 THEN
+      SELECT CONCAT( "Table ", table_number, " is already booked.") AS "Booking status";
+      ELSE 
+      SELECT CONCAT( "Table ", table_number, " is not booked.") AS "Booking status";
+    END IF;
+END//
+DELIMITER ;
 
 CALL CheckBooking("2022-12-30", 5);
 
